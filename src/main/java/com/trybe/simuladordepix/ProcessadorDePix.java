7d507dee37686c28ba1f3700ed1b047ad9fb845c
vanessabastos <vanessarsb83@gmail.com>
2023-02-03 08:31:24 -0300

    Implementação da classe Processador de Pix

--- src/main/java/com/trybe/simuladordepix/ProcessadorDePix.java
+++ src/main/java/com/trybe/simuladordepix/ProcessadorDePix.java
@@ -2,6 +2,7 @@ package com.trybe.simuladordepix;
 
 import java.io.IOException;
 
+/**Classe Processador de Pix.*/
 public class ProcessadorDePix {
 
   private final Servidor servidor;
@@ -22,6 +23,34 @@ public class ProcessadorDePix {
    *                     entre o aplicativo e o servidor na nuvem.
    */
   public void executarPix(int valor, String chave) throws ErroDePix, IOException {
-    // TODO: Implementar.
+    
+    try (Conexao conexao = servidor.abrirConexao()) {
+      validarDados(valor, chave);
+      String envioPix = conexao.enviarPix(valor, chave);
+      realizarOperacao(envioPix);
+    }
   }
+
+  private static boolean validarDados(int valor, String chave) throws ErroDePix {
+    if (valor <= 0) {
+      throw new ErroValorNaoPositivo();
+    }
+    if (chave == null || chave.isBlank()) {
+      throw new ErroChaveEmBranco();
+    }
+    return true;
+  }
+
+  private static boolean realizarOperacao(String envioPix) throws ErroDePix {
+    if (envioPix.equals("sucesso")) {
+      return true;
+    } else if (envioPix.equals("saldo_insuficiente")) {
+      throw new ErroSaldoInsuficiente();
+    } else if (envioPix.equals("chave_pix_nao_encontrada")) {
+      throw new ErroChaveNaoEncontrada();
+    } else {
+      throw new ErroInterno();
+    }
+  }
+
 }
